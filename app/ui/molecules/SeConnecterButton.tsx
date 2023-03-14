8d10f9bf282121ccaf922a0aab097e0f3d2fc7d0
Jordan Taillefer <jordan.taillefer@octo.com>
2023-03-14 14:23:42 +0100

    refacto: fix problem on rounded button

--- app/ui/molecules/SeConnecterButton.tsx
+++ app/ui/molecules/SeConnecterButton.tsx
@@ -7,7 +7,7 @@ import { RoundedButton } from "~/ui/atoms/RoundedButton"
 export const SeConnecterButton: FunctionComponent = () => {
   return (
     <Form className="inline-flex justify-center" method="post" action={`/authentication/${SocialsProvider.GOOGLE}`}>
-      <RoundedButton>
+      <RoundedButton type="submit">
         <img className="md:hidden w-4" src="/assets/icons/account.png" alt="Se connecter" />
         <span className="hidden md:block">Se connecter</span>
       </RoundedButton>
