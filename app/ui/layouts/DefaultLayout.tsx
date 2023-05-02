c1bb9ff0bdebeefbdb131d90d191df708f741732
Jordan Taillefer <jordan.taillefer@octo.com>
2023-05-02 16:45:37 +0200

    refacto(KAU-12): modification du design de la page profil

--- app/ui/layouts/DefaultLayout.tsx
+++ app/ui/layouts/DefaultLayout.tsx
@@ -1,14 +1,17 @@
-import { Outlet, useOutletContext } from "@remix-run/react"
+import { Outlet, useOutletContext } from "@remix-run/react";
 
-import { Header } from "~/ui/organisms/Header"
+import { Header } from "~/ui/organisms/Header";
 
 export const DefaultLayout = () => {
+
   const { authenticated } = useOutletContext<{ authenticated: boolean }>()
 
   return (
-    <div className="flex min-h-screen flex-col bg-background-main" data-theme="default">
+    <div
+      className="flex min-h-screen flex-col bg-background-main bg-[radial-gradient(ellipse_80%_50%_at_50%_-20%,_var(--tw-gradient-stops))] from-primary-transparent to-transparent"
+      data-theme="default"
+    >
       <Header authenticated={authenticated} />
-
       <main id="main" className="flex flex-grow flex-col overflow-x-hidden">
         <Outlet />
       </main>
---
+++ app/ui/organisms/HeaderCalendar.tsx
@@ -0,0 +1,39 @@
+const ItemDate = ({
+  date,
+  selected
+}: {
+  date: { daysAndMonth: string; days: string }
+  selected: string
+}) => {
+  return (
+    <button
+      type="submit"
+      className={date.daysAndMonth === selected ? "font-bold" : ""}
+      name="day"
+      value={date.daysAndMonth}
+    >
+      <p>{date.daysAndMonth}</p>
+      <p>{date.days}</p>
+    </button>
+  )
+}
+export const HeaderCalendar = ({ dates, selected}: {
+  dates: { daysAndMonth: string; days: string }[]
+  selected: string
+}
+) => {
+
+  return (
+    <div className="border-b border-primary-transparent mt-4 h-16 mb-4">
+      <form method="GET">
+        <ul className="flex w-full h-full text-primary">
+          {dates.map(date => (
+            <li className="w-1/5 flex items-center justify-center flex-col" key={date.daysAndMonth}>
+              <ItemDate date={date} selected={selected} />
+            </li>
+          ))}
+        </ul>
+      </form>
+    </div>
+  )
+}
