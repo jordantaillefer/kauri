aba9338646302026089e95ae78c13ad92377cadc
Jordan Taillefer <jtaillefer02@gmail.com>
2024-02-23 08:56:21 +0100

    chore: upgrade dependencies (Remix + vite)

--- app/routes/_default.planning/CalendarBody.tsx
+++ app/routes/_default.planning/CalendarBody.tsx
@@ -23,7 +23,7 @@ export const CalendarBody = ({
             onClick={() => selectDay(day)}
             className={classNames(
               isSelected(day) ? "bg-white shadow-inner" : day.isCurrentMonth ? "bg-white" : "bg-gray-50 text-gray-500",
-              "flex flex-col relative px-3 py-2"
+              "flex flex-col relative px-3 py-2 min-h-16"
             )}
           >
             <time
